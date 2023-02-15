106918736c1cef95225be9bcbcb8d522eb7221a2
Prakashgunasekaran <prakashgunasekaran@gmail.com>
2023-02-15 10:51:31 -0500

    update dag file

--- dags/tests/dag_parsing_test.py
+++ dags/tests/dag_parsing_test.py
@@ -1,9 +1,13 @@
 import subprocess
 import sys
+import os
 
 process=subprocess.run(["gcloud","composer","environments","run", "my-environment", "--location", "us-central1", "dags", "list", "--", "--subdir", "/home/airflow/gcs/data/test/"],stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 output=process.stdout
+
+#output=os.system("gcloud composer environments run my-environment --quiet --location us-central1 dags list -- --subdir /home/airflow/gcs/data/test/")
+#print(f"output:{output}")
 
 if "Error" in output:
 	print("The following dags failed to parse\n")
